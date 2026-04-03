10a99b2a024dc3cf7ef384c47c191155d9fc4ed2
Hemanth Raj <smhemanth2002@gmail.com>
2026-04-03 23:13:50 +0530

    added

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS', default='*')]
 
 # ─── Installed Apps ───
 INSTALLED_APPS = [
